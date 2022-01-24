d80d10b6c787c962373607b7bfc2d7eade381aa6
Humza Hasan <humzahasan2101@gmail.com>
2022-01-24 22:52:08 +0530

    feat: drawer added and header modified

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 import "./library/libs.js";
+
+let navHandle = document.getElementById("navHandle");
+
+navHandle.addEventListener("click", () => {
+  document.getElementById("drawerMenu").style.display == "none"
+    ? (document.getElementById("drawerMenu").style.display = "block")
+    : (document.getElementById("drawerMenu").style.display = "none");
+});
